61052782d8c00cb59b3ea8a46c3604ecfc291e19
Michael Thomas <mi.thomas@cgi.com>
2019-11-09 23:50:40 +0100

    Added Spring Boot template.

---
+++ src/test/java/de/mthix/dev/shoetracker/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.mthix.dev.shoetracker.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
